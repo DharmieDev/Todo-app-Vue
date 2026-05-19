a54492c3b837a1d1346ebf792a10ec2a96d23a5b
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-05-19 16:30:46 +0100

    Added and optimized navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import { useTaskStore } from '@/stores/taskStore';
+import { MenuRight } from '@boxicons/vue';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+
+
+const taskStore = useTaskStore()
+const { searchInput } = storeToRefs(taskStore);
+const open = ref(false);
+
+</script>
+
+<template>
+<div class="navbar  bg-gray-200 dark:bg-gray-700 shadow-sm ">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl text-[16px] md:text-2xl">
+      To-Do App
+    </a>
+  </div>
+  <input
+    type="text"
+    placeholder="Search"
+    class="input input-bordered w-auto"
+    v-model="searchInput"
+    @keydown.enter="taskStore.applySearch()"
+  />
+  <!-- Mobile Menu Icon-->
+  <button class="btn btn-ghost btn-circle">
+    <MenuRight
+    @click="open = !open"
+      class="md:hidden" width={42} height={42} />
+  </button>
+  <!-- Desktop Menu-->
+  <div>
+    <ul class="hidden md:menu md:flex md:menu-horizontal px-1 gap-2">
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/add">Add Todo</Link>
+      </li>
+      <li>
+        <button
+          class="btn hover:bg-gray-600"
+        
+        >
+          Logout
+        </button>
+      </li>
+    </ul>
+  </div>
+  <!-- Mobile Dropdown menu-->
+    <ul 
+    v-if="open"
+    class="absolute top-16 right-4 bg-base-200 rounded-box shadow-lg p-3 md:hidden menu menu-vertical">
+      <li>
+        <RouterLink to="/login">Login</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/register">Register</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/add">Add Todo</RouterLink>
+      </li>
+      <li>
+        <button
+          class="btn hover:bg-gray-600"
+          
+        >
+          Logout
+        </button>
+      </li>
+    </ul>
+</div>
+</template>

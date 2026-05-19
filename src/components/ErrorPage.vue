b6b9990b27921206eb9ba4bf5e00325dda2e2f76
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-05-19 16:32:21 +0100

    added and optimized error page & Loading spinner

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  error: Error
+}>()
+</script>
+
+<template>
+	<div>
+		<h2>Something went wrong</h2>
+		<p class="text-red-600" v-if="error">{{ error.message }}</p>
+	</div>
+</template>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div>
+		<span className="loading loading-bars loading-xl"></span>
+	</div>
+</template>

90f666c75f7de20d875788bbc00c95c804aef7e0
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-05-22 03:52:20 +0100

    Added requiresAuth and requiresGuest

--- src/router/routerTasks.ts
+++ src/router/routerTasks.ts
@@ -11,28 +11,38 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: TodoPage,
+      meta: { requiresAuth: true },
+      children: [
+        {
+          path: '/tasks/:id',
+          name: 'todo-details',
+          components: {
+            right: TodoDetails,
+          }
+        },
+        {
+          path: '/add',
+          name: 'add-todo',
+          components: {
+            right: AddTodo
+          }
+        },
+      ]
+    },
+    {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: { requiresGuest: true }
     },
     {
       path: '/register',
       name: 'register',
-      component: Register
-    },
-    {
-      path: '/add',
-      name: 'add-todo',
-      components: {
-        right: AddTodo
-      }
-    },
-    {
-      path: '/task/:id',
-      name: 'todo-details',
-      components: {
-        right: TodoDetails,
-      }
+      component: Register,
+      meta: { requiresGuest: true }
     },
     {
       path: '/:pathMatch(.*)*',
@@ -40,6 +50,18 @@ const router = createRouter({
       component: NotFound
     }
   ],
+})
+
+router.beforeEach((to) => {
+  const token = localStorage.getItem("token");
+
+  if (to.meta.requiresAuth && !token) {
+    return "/login"
+  }
+
+  if (to.meta.requiresGuest && token) {
+    return "/"
+  }
 })
 
 export default router

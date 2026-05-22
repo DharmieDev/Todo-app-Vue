d602a72e0983202c8530ad857f0d16d540c5d1ea
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-05-22 03:56:13 +0100

    updated the component to show 404 when in unexpected routes

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import router from '@/router/routerTasks';
+
 
 </script>
 
 <template>
-	<div>
-		<p>Not Found</p>
-	</div>
+<div class="fixed inset-0 flex flex-col items-center justify-center z-50">
+  <span class="text-8xl font-bold">404</span>
+  <h1>Not Found</h1>
+  <p>The page you are looking for does not exist.</p>
+  <button @click="router.push('/')" class="btn">
+    Go Home
+  </button>
+</div>
 </template>
